95cfc63323cdace7b7c6705cb0ee656089b4ea51
Anzhela Yakovchuk <anzelaakovcuk59@gmail.com>
2025-04-27 23:23:36 +0200

    fixes

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,6 @@
 
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { defineConfig, devices } from '@playwright/test';
 
 /**
--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -6,9 +6,9 @@ test('login page loads correctly', async ({ page }) => {
    
     await page.goto('/auth/login');
     
-    await page.getByTestId("email").fill('customer@practicesoftwaretesting.com');
+    await page.getByTestId("email").fill(process.env.USER_EMAIL!);
 
-    await page.getByTestId("password").fill('welcome01');
+    await page.getByTestId("password").fill(process.env.USER_PASSWORD!);
 
     await expect(page.getByTestId("login-submit")).toBeVisible();
 
@@ -27,13 +27,15 @@ test('login page loads correctly', async ({ page }) => {
 test('User can view Combination Pliers product details', async ({ page }) => {
     await page.goto('/');
   
-    await page.locator('[data-test="product-name"]', { hasText: 'Combination Pliers' }).click();
+    const product = page.getByTestId('product-name').filter({ hasText: 'Combination Pliers' });
+    await expect(product).toBeVisible(); 
+    await product.click(); 
 
     await expect(page).toHaveURL(/\/product/);
     
-    await page.locator('[data-test="product-name"]', { hasText: 'Combination Pliers' });
+    await expect(page.getByTestId("product-name")).toHaveText('Combination Pliers');
   
-    await expect(page.locator('text=14.15')).toBeVisible();
+    await expect(page.getByTestId('unit-price')).toHaveText('14.15');
   
     await expect(page.getByTestId("add-to-cart")).toBeVisible();
   });
@@ -41,13 +43,15 @@ test('User can view Combination Pliers product details', async ({ page }) => {
   
 
   test('user can add product to cart', async ({ page }) => {
-    await page.goto('https://practicesoftwaretesting.com'); 
+    await page.goto('/'); 
 
-    await page.locator('text=Slip Joint Pliers').click();
+    const product = page.getByTestId('product-name').filter({ hasText: 'Slip Joint Pliers' });
+    await expect(product).toBeVisible(); 
+    await product.click(); 
 
     await expect(page.getByTestId("product-name")).toHaveText('Slip Joint Pliers');
   
-    await expect(page.locator('text=9.17')).toBeVisible();
+    await expect(page.getByTestId('unit-price')).toHaveText('9.17');
   
     await expect(page.getByTestId("add-to-cart")).toBeVisible();
 
@@ -64,7 +68,7 @@ test('User can view Combination Pliers product details', async ({ page }) => {
 
     await page.getByTestId('cart-quantity').click();
 
-    await page.goto('https://practicesoftwaretesting.com/checkout');
+    await page.goto('/checkout');
 
     await expect(page.getByTestId("product-quantity")).toHaveValue('1');
 
